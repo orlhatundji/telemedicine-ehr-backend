7b95ddc7ab337f264a2c95303e5955e2460bec5a
Simon Babatunde <orlhatundji@gmail.com>
2024-08-13 21:17:20 +0100

    fix bug with assigning doctor to patient

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
 @Controller('admin')
@@ -6,14 +6,27 @@ export class AdminController {
   constructor(private readonly prismaService: PrismaService) {}
 
   @Post('assign-doctor')
-  async assignDoctor(@Req() data: { doctorId: number; patientId: number }) {
-    return await this.prismaService.patient.update({
+  async assignDoctor(@Body() data: { doctorId: number; patientId: number }) {
+    console.log(data);
+
+    const patient = await this.prismaService.patient.findUnique({
       where: { id: data.patientId },
-      data: {
-        assignedDoctors: {
-          connect: { id: data.doctorId },
-        },
-      },
     });
+    if (!patient) {
+      throw new HttpException('Patient not found', 404);
+    }
+    try {
+      await this.prismaService.patient.update({
+        where: { id: data.patientId },
+        data: {
+          assignedDoctors: {
+            connect: { id: data.doctorId },
+          },
+        },
+      });
+      return { message: 'Doctor assigned successfully' };
+    } catch (error) {
+      throw new HttpException('Error assigning doctor', 500);
+    }
   }
 }
